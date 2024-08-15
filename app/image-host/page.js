2a0464b0d007765d399ecd3a3b90ef3e9707d53e
Edgar <elindo586@gmail.com>
2024-08-15 12:07:50 -0400

    images

--- app/image-host/page.js
+++ app/image-host/page.js
@@ -3,16 +3,76 @@
 import styles from "./styles.module.css";
 import Image from "next/image";
 
-export default function Page({ children }) {
+export default function Page() {
   return (
     <div>
-      <div className={styles.facebook}>
+      <div className={styles.wrap}>
         <Image
           fill={true}
           src="/images/social-icons/facebook.png"
-          id="facebook"
           alt="facebook icon"
           title="facebook icon"
+          id="facebook"
+          // width={16}
+          // height={16}
+          sizes="100vw"
+        />
+      </div>
+      <div className={styles.wrap}>
+        <Image
+          fill={true}
+          src="/images/social-icons/instagram.png"
+          alt="facebook icon"
+          title="facebook icon"
+          id="facebook"
+          // width={16}
+          // height={16}
+          sizes="100vw"
+        />
+      </div>
+      <div className={styles.wrap}>
+        <Image
+          fill={true}
+          src="/images/social-icons/linkedin.png"
+          alt="facebook icon"
+          title="facebook icon"
+          id="facebook"
+          // width={16}
+          // height={16}
+          sizes="100vw"
+        />
+      </div>
+      <div className={styles.wrap}>
+        <Image
+          fill={true}
+          src="/images/social-icons/snapchat.png"
+          alt="facebook icon"
+          title="facebook icon"
+          id="facebook"
+          // width={16}
+          // height={16}
+          sizes="100vw"
+        />
+      </div>
+      <div className={styles.wrap}>
+        <Image
+          fill={true}
+          src="/images/social-icons/tiktok.png"
+          alt="facebook icon"
+          title="facebook icon"
+          id="facebook"
+          // width={16}
+          // height={16}
+          sizes="100vw"
+        />
+      </div>
+      <div className={styles.wrap}>
+        <Image
+          fill={true}
+          src="/images/social-icons/youtube.png"
+          alt="facebook icon"
+          title="facebook icon"
+          id="facebook"
           // width={16}
           // height={16}
           sizes="100vw"

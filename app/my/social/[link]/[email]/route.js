94ccbb1e6ba72ca8b5ba64584eb9b760ee3effcb
Edgar <elindo586@gmail.com>
2024-08-18 13:20:32 -0400

    email

--- app/my/social/[link]/[email]/route.js
+++ app/my/social/[link]/[email]/route.js
@@ -1,7 +1,8 @@
 import nodemailer from "nodemailer";
 import { NextResponse } from "next/server";
 
-export async function GET(_, { params }) {
+export async function GET(request, { params }) {
+  // Assuming `params` is used to extract `linkClick`
   const linkClick = params.link;
   const userEmail = params.email;
 
@@ -16,48 +17,44 @@ export async function GET(_, { params }) {
   const mailData = {
     from: { name: "Edgar Lindo1", address: process.env.EMAIL2 },
     to: "edgar@tu.biz",
-    subject: `We got feedback from ${userEmail}`,
+    subject: `We got feedback from ${linkClick}`,
     text: ``,
     html: `<div>
-            <di> Hello There,</div>
-            <div> This is a click.  ${userEmail} </div>
-            <div> he clicked on ${linkClick} </div>
+            <div>Hello There,</div>
+            <div>This is a click. ${linkClick}</div>
+            <div>He clicked on ${userEmail}</div>
           </div>`,
   };
-  await new Promise((resolve, reject) => {
-    transporter.sendMail(mailData, function (err, info) {
-      if (err) {
-        console.log(err);
-        reject(err);
-      } else {
-        console.log("email sent");
-        resolve(info);
-      }
-    });
-  });
 
-  if (linkClick === "facebook") {
-    return NextResponse.redirect(
-      new URL("https://www.facebook.com", request.url)
-    );
-  } else if (linkClick === "instagram") {
-    return NextResponse.redirect(
-      new URL("https://www.instagram.com", request.url)
-    );
-  } else if (linkClick === "linkedin") {
-    return NextResponse.redirect(
-      new URL("https://www.linkedin.com", request.url)
-    );
-  } else if (linkClick === "snapchat") {
-    return NextResponse.redirect(
-      new URL("https://www.snapchat.com", request.url)
-    );
-  } else if (linkClick === "tiktok") {
-    return NextResponse.redirect(
-      new URL("https://www.tiktok.com", request.url)
-    );
-  } else
-    return NextResponse.redirect(
-      new URL("https://www.youtube.com", request.url)
-    );
+  try {
+    await transporter.sendMail(mailData);
+    console.log("email sent");
+  } catch (err) {
+    console.error("Failed to send email:", err);
+    // You might want to handle the error, return an error response, etc.
+    return NextResponse.error();
+  }
+
+  // Ensure you use `request.url` if `request` is used
+  const redirectUrl = new URL("https://www.youtube.com", request.url);
+
+  switch (linkClick) {
+    case "facebook":
+      redirectUrl.href = "https://www.facebook.com";
+      break;
+    case "instagram":
+      redirectUrl.href = "https://www.instagram.com";
+      break;
+    case "linkedin":
+      redirectUrl.href = "https://www.linkedin.com";
+      break;
+    case "snapchat":
+      redirectUrl.href = "https://www.snapchat.com";
+      break;
+    case "tiktok":
+      redirectUrl.href = "https://www.tiktok.com";
+      break;
+  }
+
+  return NextResponse.redirect(redirectUrl);
 }

f3a89b0709cdadd7af5efff6780f72ab622161f5
Edgar <elindo586@gmail.com>
2024-08-20 11:56:22 -0400

    other test

---
+++ app/my/social/[link]/[email]/page.js
@@ -0,0 +1,32 @@
+// app/social/[link]/[email]/page.js
+import { useEffect } from "react";
+
+export default function SocialPage({ params }) {
+  const { link, email } = params;
+
+  useEffect(() => {
+    const redirectToSocial = async () => {
+      // Send the email notification
+      await fetch("/trackclick", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ social: link, email }),
+      });
+
+      // Redirect to the appropriate social media site
+      if (link === "facebook") {
+        window.location.href = "https://www.facebook.com";
+      } else if (link === "youtube") {
+        window.location.href = "https://www.youtube.com";
+      } else {
+        console.error("Unknown social media link");
+      }
+    };
+
+    redirectToSocial();
+  }, [link, email]);
+
+  return <div>Redirecting...</div>;
+}

fbe7c3e976edec1f7006f38f967b003ce40bd0d8
Edgar <elindo586@gmail.com>
2024-09-03 16:18:25 -0400

    social

--- app/my/social/[link]/[email]/[campid]/route.js
+++ app/my/social/[link]/[email]/[campid]/route.js
@@ -15,10 +15,32 @@ export async function GET(request, { params }) {
   const seconds = d.getSeconds();
 
   const date = ` ${year}/${month}/${days} at ${hour}:${minutes}:${seconds}s`;
+
   try {
-    const result =
-      await sql`INSERT INTO Social ( social, email, campId, date ) VALUES ( ${social}, ${userEmail}, ${campId}, ${date};`;
-    return NextResponse.json({ result }, { status: 200 });
+    await sql`INSERT INTO social (social, email, campId, date) VALUES ( ${social}, ${userEmail}, ${campId}, ${date});`;
+    const redirectUrl = new URL(
+      "https://www.youtube.com/@EdgarLindo",
+      request.url
+    );
+
+    switch (linkClick) {
+      case "facebook":
+        redirectUrl.href = "https://www.facebook.com";
+        break;
+      case "instagram":
+        redirectUrl.href = "https://www.instagram.com/mredgarlindo";
+        break;
+      case "linkedin":
+        redirectUrl.href = "https://www.linkedin.com/in/edgarlindo/";
+        break;
+      case "snapchat":
+        redirectUrl.href = "https://www.snapchat.com/add/elindo586";
+        break;
+      case "tiktok":
+        redirectUrl.href = "https://www.tiktok.com/@theedgarlindo";
+        break;
+    }
+    return NextResponse.redirect(redirectUrl);
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }

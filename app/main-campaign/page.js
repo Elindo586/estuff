dcb9cba5e5bb2f949bafde289b3603796653a007
Edgar <elindo586@gmail.com>
2024-09-03 15:28:44 -0400

    test

--- app/main-campaign/page.js
+++ app/main-campaign/page.js
@@ -2,7 +2,7 @@
 
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import quotes from "../../thejsons/emails3200.json";
+import quotes from "../../thejsons/test-emails.json";
 
 export default function Page() {
   const handleSubmit = () => {

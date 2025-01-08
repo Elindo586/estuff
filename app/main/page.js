581391f310799d82d2cf9e8d6f51fb10be5aba93
Edgar Lindo <info@tu.biz>
2025-01-08 10:28:50 -0600

    quotes

--- app/main/page.js
+++ app/main/page.js
@@ -2,7 +2,7 @@
 
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import quotes from "../../thejsons/list.json";
+import quotes from "../../thejsons/test-quote.json";
 
 export default function Page() {
   const handleSubmit = () => {

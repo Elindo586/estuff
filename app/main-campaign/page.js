befa45c42b77cf4d2aae9246cdc71b23c22dff3e
Edgar Lindo <info@tu.biz>
2025-01-16 08:42:03 -0600

    main-c

--- app/main-campaign/page.js
+++ app/main-campaign/page.js
@@ -64,7 +64,7 @@ export default function Page() {
       );
       console.log(arrayIndex, quotes.length);
 
-      if (arrayIndex === quotes.lenght) {
+      if (arrayIndex === quotes.length) {
         console.log("We are done!");
 
         clearInterval(myIntervals);

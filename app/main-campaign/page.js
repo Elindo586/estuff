b3b5643c0c949963304d607fe86a9e98b3124cb1
Edgar Lindo <info@tu.biz>
2025-02-07 13:09:55 -0600

    test

--- app/main-campaign/page.js
+++ app/main-campaign/page.js
@@ -2,7 +2,7 @@
 
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import quotes from "../../thejsons/test-emails.json";
+import quotes from "../../thejsons/2-7-25-distributorspanish.json";
 
 export default function Page() {
   const handleSubmit = () => {
